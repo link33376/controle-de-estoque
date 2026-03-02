95dc05d5216b720caab978a9cfaf4a9050908267
joquebedebarboza-ux <joquebedebarboza@gmail.com>
2026-03-02 17:47:34 -0300

    finalizados a chamada de banco dados  e configuração do haader

--- src/banco.php
+++ src/banco.php
@@ -15,22 +15,23 @@
 try {
 //tentativa de conexão com banco de dados
 //codigo...
-//conecção com o banco de dados
+//conexão com o banco de dados
 // classe  PDO é usanda acessar varios tipos de banco de dados, nesse caso mysql
-/*DSN DATA SOURCE NAME, é a string de conexão, onde se passa o tipo do banco de dados, o host e o nome do banco de dados 
-o host e o nome do banco de dados*/
+/*DSN DATA SOURCE NAME, é a string de conexão, onde se passa o tipo do banco de dados, o host e o nome do banco de dados*/
 
-    $conexao = new PDO("mysql:host=$servidor;
-    dbname=$banco",
-     $usuario,
-     $senha);
+    $conexao = new PDO("mysql:host=$servidor; dbname=$banco", $usuario,$senha);
 
+     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     //echo "Conexao com o banco de dados estabelecida com sucesso!";
+
+     
     } catch (\Throwable $erro){
-        //lançavel  seve paras qualque tipo de erro  ou coneceção
+        //lançavel serve para qualquer tipo de erro ou concessão
         //captura de erro caso a conexão falhe
+        die("erro ao conectar com o banco de dados: " . $erro->getMessage());
         
 
-     }
+    }
 
     
     
